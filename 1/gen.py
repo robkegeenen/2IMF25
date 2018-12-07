1187511d5ffd3e53f2c14c41efb280b9ac2016d1
s135755 <j.mols@student.tue.nl>
2018-12-07 13:39:51 +0100

    Added 1(b)

--- 1/gen.py
+++ 1/gen.py
@@ -8,18 +8,19 @@ aT = 4
 cT = 8500
 pT = 8
 pallets = [
-  {"name": "C", "fullname": "Cheese"      , "w": 700 , "d": 4 , "a": False},
-  {"name": "B", "fullname": "Beer"        , "w": 1300, "d": -1, "a": True },
-  {"name": "W", "fullname": "Wine"        , "w": 1000, "d": 8 , "a": True },
-  {"name": "D", "fullname": "Soft drinks" , "w": 1500, "d": 10, "a": False},
-  {"name": "P", "fullname": "Potato chips", "w": 100 , "d": 5 , "a": False},
+  {"name": "C", "fullname": "Cheese"      , "w": 700 , "d": 4 , "max": -1, "a": False},
+  {"name": "B", "fullname": "Beer"        , "w": 1300, "d": -1, "max": -1, "a": True },
+  {"name": "W", "fullname": "Wine"        , "w": 1000, "d": 8 , "max": -1, "a": True },
+  {"name": "D", "fullname": "Soft drinks" , "w": 1500, "d": 10, "max": 2 , "a": False},
+  {"name": "P", "fullname": "Potato chips", "w": 100 , "d": 5 , "max": -1, "a": False},
 ]
+irange = range(1, T+1)
 
 #########################
 ### Declare constants ###
 #########################
 for j, p in enumerate(pallets):
-  for i in range(1, (T+1)):
+  for i in irange:
     print("(declare-const %s%d %s)" % (p["name"], i, domain))
   if p["d"] < 0:
     print("(declare-const %s %s)" % (p["name"], domain))
@@ -33,7 +34,7 @@ print("(and")
 ###########################################
 ### Max truck weights and pallet counts ###
 ###########################################
-for i in range(1, (T+1)):
+for i in irange:
   fmtw = "(>= %d (+" + " (* %s%d %d)"*len(pallets) + "))"
   valw = (cT,)
   fmtc = "(>= %d (+" + " %s%d"*len(pallets) + "))"
@@ -47,7 +48,7 @@ for i in range(1, (T+1)):
 #############################
 ### Delivery Requirements ###
 #############################
-fmt = string.join(["(= %d (+"] + [" %s%%d" % x["name"] if x["d"] >= 0 else "" for x in pallets] + ["))"], '')
+fmt = "".join(["(= %d (+"] + [" %s%%d" % x["name"] if x["d"] >= 0 else "" for x in pallets] + ["))"])
 for j, p in enumerate(pallets):
   if p["d"] >= 0:
     print("(= %d (+" % p["d"], end = "")
@@ -68,10 +69,18 @@ for i in range((aT+1), (T+1)):
 ##################################
 ### Non-negative pallet counts ###
 ##################################
-for i in range(1, (T+1)):
+for i in irange:
   for j, p in enumerate(pallets):
     print("(>= %s%d 0)" % (p["name"], i))
 
+############################
+### Pallet type max load ###
+############################
+for j, p in enumerate(pallets):
+  if p["max"] >= 0:
+    for i in irange:
+      print("(<= %s%d %d)" % (p["name"], i, p["max"]))
+    
 ##########################
 ### Close requirements ###
 ##########################
