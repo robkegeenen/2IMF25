b3b2c8848d8ebdcc4c7980fe8e6028a22ac3fa04
Röbke Geenen <robke@insanity.insaniac.net>
2019-01-04 02:10:34 +0100

    Added comments to assignment 6 NuSMV generation script and reordered it.

--- 6/gen_nusmv.py
+++ 6/gen_nusmv.py
@@ -6,17 +6,26 @@ bgn = [7]
 end = [7, 16]
 stepguess = 4180
 
+#############################
+### Variable declarations ###
+#############################
 print("MODULE main")
 print("VAR")
 print("s: 0..%d;" % stepguess)
 for i in irange:
   print("a%d: boolean;" % i)
 
+######################
+### Initial values ###
+######################
 print("\nINIT")
 for i in irange:
   print("%sa%d &" % ("" if i in bgn else "!", i))
 print("s = 0")
 
+###################################################
+### Transitions (only if 's' does not overflow) ###
+###################################################
 print("\nTRANS")
 print("case")
 print("  s < %d:" % stepguess)
@@ -24,6 +33,26 @@ print("    next(s) = s + 1 & (")
 def rest(f, L):
   return(string.join(["%snext(a%d) = a%d" % (f, x, x) for x in L], " &\n"))
 
+##############
+### Rule 1 ###
+##############
+print("      (")
+print("        next(a0) = TRUE &")
+print(rest("        ", irange[1:]))
+print("      ) |")
+
+##############
+### Rule 2 ###
+##############
+print("      (")
+print("        next(a0) = a1 &")
+print("        next(a1) = a0 &")
+print(rest("        ", irange[2:]))
+print("      ) |")
+
+##############
+### Rule 3 ###
+##############
 for i in irange[2:]:
   print("      (")
   print("        (a%d & a%d) &" % (i-2, i-1))
@@ -31,26 +60,22 @@ for i in irange[2:]:
   print("          next(a%d) = FALSE &" % (i-1))
   print("          next(a%d) = TRUE &" % i)
   print(rest("          ", [x for x in irange if x not in [i-2, i-1, i]]))
-  print("      ) |")
+  print("      )" + ("" if i == irange[-1] else " |"))
 
-print("      (")
-print("        next(a0) = TRUE &")
-print(rest("        ", irange[1:]))
-print("      ) |")
-
-print("      (")
-print("        next(a0) = a1 &")
-print("        next(a1) = a0 &")
-print(rest("        ", irange[2:]))
-print("      )")
-
+#######################################
+### If 's' would overflow then bail ###
+#######################################
 print("    );")
 print("  TRUE:")
 print("    next(s) = s;")
 print("esac")
 
-for guess in [stepguess, stepguess - 1]:
-  print("\nSPEC EF (s > 0 &")
+###############################################
+### Required state reachable in the guessed ###
+### number of steps but not in one less?    ###
+###############################################
+for guess, neg in {stepguess: "", stepguess - 1: "!"}.iteritems():
+  print("\nSPEC %s EF (s > 0 &" % neg)
   for i in end:
     print("  a%d &" % i)
   print("  s <= %d)" % guess)
